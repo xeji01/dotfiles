3ccd0f3bf783775f30f6e0e1e43a1f4ce8a95b55
xeji01 <xejireacts@gmail.com>
2024-12-14 18:25:59 +0700

    Add files via upload

---
+++ config/nvim/init.lua
@@ -0,0 +1,4 @@
+require("config.lazy")
+
+vim.wo.number = true
+
---
+++ config/nvim/lua/plugins/pywal16.lua
@@ -0,0 +1,12 @@
+return {
+
+   {
+  'uZer/pywal16.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("pywal16")
+  end,
+ },
+}
+
